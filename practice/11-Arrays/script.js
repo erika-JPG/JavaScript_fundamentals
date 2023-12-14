86c1182053ed3e0315cb254258f9bbd1945a9703
Erika J Ponce <erikaponce08@hotmail.com>
2023-12-13 21:11:37 -0800

    challenge 4

--- practice/11-Arrays/script.js
+++ practice/11-Arrays/script.js
@@ -428,5 +428,159 @@ const bankDepositSum = accounts.flatMap(acc => acc.movements).filter(mov => mov 
 console.log(bankDepositSum);
 
 // #2 Count how many deposits are over 1000
-const numDeposits = accounts.flatMap(acc => acc.movements).filter(mov => mov > 1000).length;
+// Using flatMap
+// const numDeposits = accounts.flatMap(acc => acc.movements).filter(mov => mov > 1000).length;
+// Using reduce
+const numDeposits = accounts.flatMap(acc => acc.movements).reduce((count, cur) => (cur >= 1000 ? count + 1 : count ) ,0)
 console.log(numDeposits)
+
+//#3 Creating a new object wich contains the sum of the deposits and withdrawals
+
+// in arrow functions the value is implicitly returned when we don't have a function body (curly braces), so we need to manually return the value
+const {dpsts, wthwls} = accounts.flatMap(acc => acc.movements).reduce((sums, curr) => { 
+  // curr > 0 ? sums.dpsts += curr : sums.withrawals += curr;
+  //  fixing the duplication from above
+  sums[curr > 0 ? 'dpsts' : 'wthwls'] += curr;
+  return sums;
+
+}, { dpsts: 0, wthwls: 0 })
+
+console.log(dpsts);
+console.log(wthwls);
+
+/*
+Coding Challenge #4
+Julia and Kate are still studying dogs, and this time they are studying if dogs are
+eating too much or too little.
+Eating too much means the dog's current food portion is larger than the
+recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10%
+above and 10% below the recommended portion (see hint).
+Your tasks:*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+  
+/*
+1. Loop over the 'dogs' array containing dog objects, and for each dog, calculate
+the recommended food portion and add it to the object as a new property. Do
+not create a new array, simply loop over the array. Forumla:
+recommendedFood = weight ** 0.75 * 28. (The result is in grams of
+food, and the weight needs to be in kg)*/
+
+// dogs.forEach(e => { 
+//   const recommendedFood = e.weight ** 0.75 * 28;
+//   e.recommendedFood = recommendedFood;
+//   console.log(recommendedFood)
+// });
+
+//jonas
+dogs.forEach(dog => dog.recFood = Math.trunc(dog.weight ** 0.75 * 28));
+
+console.log(dogs)
+
+
+/*2. Find Sarah's dog and log to the console whether it's eating too much or too
+little. Hint: Some dogs have multiple owners, so you first need to find Sarah in
+the owners array, and so this one is a bit tricky (on purpose)*/
+
+// const saraDog = dogs.forEach(e => {
+//   if (e.owners.includes('Sarah')) {
+//     const range = e.recommendedFood - e.curFood < 0 ? `too much` : `too little`;
+//     console.log(`Sarah's dog is eating ${range}`)
+//   }
+// });
+
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(`Sarah's dog is eating too ${dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'}`)
+
+/*3. Create an array containing all owners of dogs who eat too much
+('ownersEatTooMuch') and an array with all owners of dogs who eat too little
+('ownersEatTooLittle').*/
+
+// let ownersEatTooMuch = [], ownersEatTooLittle = [];
+
+// dogs.forEach(e => {
+//   e.recommendedFood - e.curFood < 0 ? ownersEatTooMuch.push(e) : ownersEatTooLittle.push(e);
+// })
+// console.log(ownersEatTooMuch);
+// console.log(ownersEatTooLittle);
+
+const ownersEatTooMuch = dogs.filter(dog => dog.curFood > dog.recFood).flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch)
+const ownersEatTooLittle = dogs.filter(dog => dog.curFood < dog.recFood).flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle)
+
+/*4. Log a string to the console for each array created in 3., like this: "Matilda and
+Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat
+too little!"*/
+
+// function owners(array) {
+//   let ownList = [];
+//   let quantity = '';
+//   array.forEach(e => {
+//     quantity = e.recommendedFood - e.curFood < 0 ? `too much` : `too little`;
+
+//     ownList.push(e.owners)
+//   })
+//   const allOwners = ownList.flatMap(own => own).join(' and ').trim();
+//   console.log(`${allOwners}'s dogs eat ${quantity}`);
+// }
+
+// owners(ownersEatTooMuch);
+// owners(ownersEatTooLittle);
+
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+/*5. Log to the console whether there is any dog eating exactly the amount of food
+that is recommended (just true or false)*/
+
+// dogs.forEach(e => {
+//   const quantity = e.recommendedFood - e.curFood == 0 ? true : false;
+//   console.log(quantity);
+// })
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+/*6. Log to the console whether there is any dog eating an okay amount of food
+(just true or false)*/
+
+// dogs.forEach(e => {
+//   const quantity = ((e.current > (e.recommendedFood * 0.90)) && (e.current < (recommendedFood *
+//     1.10)))  ? true : false;
+//   console.log(quantity);
+// })
+
+const checkEatingOk = dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+console.log(dogs.some(checkEatingOk));
+
+/*7. Create an array containing the dogs that are eating an okay amount of food (try
+to reuse the condition used in 6.)*/
+
+console.log(dogs.filter(checkEatingOk));
+
+/*8. Create a shallow copy of the 'dogs' array and sort it by recommended food
+portion in an ascending order (keep in mind that the portions are inside the
+array's objects )*/
+
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
+
+
+/*The Complete JavaScript Course 26
+Hints:
+§ Use many different tools to solve these challenges, you can use the summary
+lecture to choose between them �
+§ Being within a range 10% above and below the recommended portion means:
+current > (recommended * 0.90) && current < (recommended *
+1.10). Basically, the current portion should be between 90% and 110% of the
+recommended portion.
+*/
+
+//Test data:
